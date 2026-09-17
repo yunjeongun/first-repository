8a6363e26028b590f17e2def833cbe4810ecc70d
yunjeongun <yunjeongun07@gmail.com>
2026-09-17 13:29:30 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 def print_hello():
     print("Hello World!!")
 
+def print_KIT():
+    print("Kumoh National Institute of Technology.")
+
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
