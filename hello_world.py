5c6fe3c5b2101b746360dfcf74a6d1958fe8e843
yunjeongun <yunjeongun07@gmail.com>
2026-09-17 13:38:22 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
-def print_hello():
-    print("Hello World!!")
+def add(x, y):
+    a = x + y
+    return a
 
 def print_KIT():
-    print("Kumoh National Institute of Technology.")
+    print("KIT")
 
 if __name__ == '__main__':
     print_KIT()
-    print_hello()
+    print( print_hello(1,2) )
